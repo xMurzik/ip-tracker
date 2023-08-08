674dd5b7c63fed27a000bb88402d223de447d743
xMurzik <murzik6667@gmail.com>
2023-08-08 16:11:02 +0300

    edited header

--- src/components/location-into-tab.tsx/location-info-tab.tsx
+++ src/components/location-into-tab.tsx/location-info-tab.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
+import { Col, Row, Container } from 'react-bootstrap';
+import styles from './location-info-tab.module.scss';
+
+const LIST = ['IP ADDRES', 'LOCATION', 'TIMEZONE', 'ISP'];
 
 const LocationInfoTab: React.FC = () => {
   return (
-    <ListGroup horizontal="md">
-      <ListGroup.Item>This ListGroup</ListGroup.Item>
-      <ListGroup.Item>renders horizontally</ListGroup.Item>
-      <ListGroup.Item>on lg</ListGroup.Item>
-      <ListGroup.Item>and above!</ListGroup.Item>
-    </ListGroup>
+    <Container className={styles.mainCont}>
+      <Row xs={1} md={4}>
+        {LIST.map((el, id) => (
+          <Col
+            key={id}
+            className={`${
+              id !== 3 ? styles.oneElemList : styles.oneElemListLast
+            }`}
+          >
+            <h4 style={{ whiteSpace: 'nowrap' }}>{el}</h4>
+            <p>192,2323,234,123</p>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
